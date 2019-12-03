26925fc3e0e7c32bb4154e2ac5e8e393c73a7cae
Melanie <mejr5b@gmail.com>
2019-12-02 20:18:52 -0600

    start of studio. building base class

---
+++ src/org/launchcode/studio7/BaseDisc.java
@@ -0,0 +1,10 @@
+package org.launchcode.studio7;
+
+public abstract class BaseDisc implements OpticalDisc, Comparable{
+
+    private int minSpinRate;
+    private int maxSpinRate;
+
+    public
+
+}
